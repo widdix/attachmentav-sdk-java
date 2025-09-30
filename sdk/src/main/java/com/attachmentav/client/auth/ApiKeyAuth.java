63c2a64682bac30252faa784138167f1e7bb5275
Michael Wittig <michael@widdix.de>
2025-09-30 11:59:23 +0200

    feat: add whomai endpoint

--- sdk/src/main/java/com/attachmentav/client/auth/ApiKeyAuth.java
+++ sdk/src/main/java/com/attachmentav/client/auth/ApiKeyAuth.java
@@ -20,7 +20,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-07-16T10:01:55.392837+02:00[Europe/Berlin]", comments = "Generator version: 7.14.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-09-30T11:58:19.717018+02:00[Europe/Berlin]", comments = "Generator version: 7.16.0")
 public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
